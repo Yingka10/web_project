c15016ca745b454920df043d57647b939176fbcf
Yingka10 <jennychou0710@gmail.com>
2025-04-21 22:57:42 +0800

    railway postgreSQL

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,17 +74,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  #PostgreSQL
-        'NAME': 'goods',  #資料庫名稱
-        'USER': 'postgres',  #資料庫帳號
-        'PASSWORD': 'jenny940710',  #資料庫密碼
-        'HOST': 'localhost',  #Server(伺服器)位址
-        'PORT': '5432'  #PostgreSQL Port號
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ORHXvgDhzGmwbitOeAvKMSNQwViVmajw',
+        'HOST': 'maglev.proxy.rlwy.net',
+        'PORT': '20216',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
