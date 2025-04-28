d6cc3009ff3154466e70c29e0aa53d64a8c842e8
Yingka10 <jennychou0710@gmail.com>
2025-04-28 11:07:09 +0800

    加admin

--- mywebsite/admin.py
+++ mywebsite/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from mywebsite.models import Post, Category
+from mywebsite.models import Post, Category, CustomUser, Notification, Reservation, Rating, ProductImage
 
-# Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(CustomUser)
+admin.site.register(Notification)
+admin.site.register(Reservation)
+admin.site.register(Rating)
+admin.site.register(ProductImage)
