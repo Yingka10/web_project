faa488b99648c3dc1c5c58c6838a53daa55347ff
Jason <ajasonkuo0713@gmail.com>
2025-05-18 02:00:29 +0800

    通知下拉選單

--- mywebsite/context_processors.py
+++ mywebsite/context_processors.py
@@ -1,4 +1,5 @@
 from .models import Category
+from .models import Notification
 
 def categories_processor(request):
     categories = Category.objects.all()
@@ -6,13 +7,17 @@ def categories_processor(request):
 
 def notification_context(request):
     if request.user.is_authenticated:
-        unread_count = request.user.notifications.filter(is_read=False).count()
-        notifications = request.user.notifications.all()[:5]
+        # 先取得使用者所有通知（未切片）
+        all_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
+        # 最新 3 筆通知
+        recent_notifications = all_notifications[:3]
+        # 未讀數量
+        unread_count = all_notifications.filter(is_read=False).count()
     else:
         unread_count = 0
-        notifications = []
+        recent_notifications = []
 
     return {
         'unread_count': unread_count,
-        'notifications': notifications,
+        'recent_notifications': recent_notifications,
     }
